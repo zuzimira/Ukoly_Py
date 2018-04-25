a96dabe89f598f261c61899c339fe1a52f5b5703
Zuzana Filova <zuzana.filova@volny.cz>
2018-04-25 14:47:41 +0200

    Provedeny opravy po kontrole.

--- Seznamy_zvirat/seznamy_zvirat.py
+++ Seznamy_zvirat/seznamy_zvirat.py
@@ -6,51 +6,51 @@ seznam_zvirat1 = ["holubicka","prasatko","koza", "kun", "pes", "husa", "kocka", 
 seznam_zvirat2 = ["vul","koza", "pes", "husa", "kralik", "had", "kruta", "krava", "slepice","kachna", "osel"]
 
 
-def kratka_slova (seznam_zvirat1):
-    "Vraci zvire kratsi nez 5 pismen"
-    
+def kratka_slova (seznam):
+    "Vraci slova kratsi nez 5 pismen"
+
     kratsi = []
-    for zvire in seznam_zvirat1:
+    for zvire in seznam:
         if len(zvire)<5:
             kratsi.append(zvire)
-    
-    return (kratsi)
-    
-def prvni_k (seznam_zvirat1):
-    "Vraci zvire zacinajici na K"
+
+    return kratsi
+
+def prvni_k (seznam):
+    "Vraci slova zacinajici na K"
     na_k = []
-    for zvire in seznam_zvirat1:
-        
+    for zvire in seznam:
+
         if zvire.startswith("k"):
             na_k.append(zvire)
-    
-    return (na_k)
 
-def over (seznam_zvirat1, slovo):
+    return na_k
+
+def over (seznam, slovo):
     "Vrati, zda je zadane slovo v seznamu"
-    return slovo in seznam_zvirat1
+    return slovo in seznam
 
-def porovnej (seznam_zvirat1, seznam_zvirat2):
-#    """Porovna dva seznamy a vrati tri. 
-#    V prvnim jsou zvirata, kter8 jsou v obou seznamech,
-#    ve druhem ta, co jsou pouze v prvnim seznamu 
+def porovnej (seznam1, seznam2):
+#    """Porovna dva seznamy a vrati tri.
+#    V prvnim jsou slova, kter8 jsou v obou seznamech,
+#    ve druhem ta, co jsou pouze v prvnim seznamu
 #    a ve tretim ta, co jsou pouze ve druhem seznamu."""
 
     seznam_oba = []
     seznam_jen1 = []
     seznam_jen2 = []
     vysledek = []
-   
-    for zvire in seznam_zvirat1:
-        if zvire in seznam_zvirat2:
+
+    for zvire in seznam1:
+        if zvire in seznam2:
             seznam_oba.append(zvire)
         else:
             seznam_jen1.append(zvire)
-            
-    for zvire in seznam_zvirat2:
+
+    for zvire in seznam2:
         if zvire not in seznam_zvirat1:
             seznam_jen2.append(zvire)
-            
+
     return(seznam_oba, seznam_jen1, seznam_jen2 )
 
 def serad(seznam):
@@ -60,17 +60,17 @@ def serad(seznam):
 
 def serad2(seznam):
     """Seradi seznam dle druheho pismene"""
-    
+
     seznam_dvojic = []                 #Radi podle druheho pismena, jsou li stejna, nasleduje prvni
-    for zvire in seznam_zvirat1:
-        seznam_dvojic.append([zvire[1],zvire])      #vytvori dvojice a seradi podle klice, klic je druhe pismeno
+    for zvire in seznam:
+        seznam_dvojic.append((zvire[1],zvire))      #vytvori dvojice a seradi podle klice, klic je druhe pismeno
     seznam_dvojic.sort()
-    
+
     seznam_klic = []
-    for i in range (len(seznam_dvojic)-1):          #vytvori novy seznam zvirat, serazeny dle klice 
-        seznam_klic.append(seznam_dvojic[i][1])
+    for dvojice in seznam_dvojic:          #vytvori novy seznam zvirat, serazeny dle klice
+        seznam_klic.append(seznam_dvojic[seznam_dvojic.index(dvojice)][1])
     return seznam_klic
- 
+
 
 
 #volani funkci
