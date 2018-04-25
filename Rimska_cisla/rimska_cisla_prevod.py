f4a2d88477c7eb5a0b54759b8289094f4611765a
Zuzana Filova <zuzana.filova@volny.cz>
2018-04-25 14:45:22 +0200

    Provedeny opravy po kontrole ukolu.

--- Rimska_cisla/rimska_cisla_prevod.py
+++ Rimska_cisla/rimska_cisla_prevod.py
@@ -1,3 +1,13 @@
+
+
+#rimska_cisla
+#- celkový postup bezva, neplatné kombinace by případně šly rozdělit do skupinek/obecnějších pravidel a kontrolovat postupně
+#- bylo by dobré zbavit se globálních proměnných:
+
+
+#    - arabske_cislice , a po tom, co je naplníš a z funkce vrátíš, uloženy do proměnné, se kterou se bude dál pracovat v prepocitej
+#- ve funkci preved můžeš opět procházet vstupní řetězec přímo a místo for i in range(len(vstup)): if vstup[i] == ... psát for znak in vstup: if znak == ...
+#- funkce prepocitej by mohla vracet jen výsledné číslo a o výpis věty by ses postarala až po jeho získání
 #I=1                #Hodnoty jednotlivych cisel
 #V=5
 #X=10
@@ -15,11 +25,7 @@
 #CM = 900
 
 #nepovolene kombinace
-nepovolene_kombinace = ["IIV", "IIX", "XXL", "XXC", "CCD", "CCM","IXIX","XLXL", "XCXC","CDCD","CMCM","IVIV","VV","LL","DD","IL" , "IC" , "ID" , "IM" , "XD", "XM" , "VX" , "VL" , "VC" , "VD" , "LC", "LD" , "LM" , "DM" ]
-
-rimske_cislice = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
-arabske_cislice = []        #seznam arabskych cislic, jak jdou v cisle za sebou
-seznam_chyb = []            #seznam spatnych kombinaci znaku v zadanem cisle
+#["IIV", "IIX", "XXL", "XXC", "CCD", "CCM","IXIX","XLXL", "XCXC","CDCD","CMCM","IVIV","VV","LL","DD","IL" , "IC" , "ID" , "IM" , "XD", "XM" , "VX" , "VL" , "VC" , "VD" , "LC", "LD" , "LM" , "DM" ]
 
 
 rimske_cislo = input("Zadej rimske cislo:  ")    # Zadani rimskeho cisla
@@ -30,7 +36,11 @@ rimske_cislo = rimske_cislo.upper()
 def over_vstup(uzivatel_vstup):
     """Overi, zda zadany retezec je rimske cislo ve spravnem formatu
     Pokud neni vraci False a tiskne seznam chyb, pokud ano, vraci True"""
+    nepovolene_kombinace = ["IIV", "IIX", "XXL", "XXC", "CCD", "CCM","IXIX","XLXL", "XCXC","CDCD","CMCM","IVIV","VV","LL","DD","IL" , "IC" , "ID" , "IM" , "XD", "XM" , "VX" , "VL" , "VC" , "VD" , "LC", "LD" , "LM" , "DM" ]
 
+    rimske_cislice = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+            #seznam arabskych cislic, jak jdou v cisle za sebou
+    seznam_chyb = []            #seznam spatnych kombinaci znaku v zadanem cisle
     for i in rimske_cislice:            #kontrola poctu jednotlivych znaku bezprostredne za sebou
         if 4*i in uzivatel_vstup:
             print('Rimske cislo nesmi obsahovat ctyri stejne znaky za sebou.')
@@ -54,28 +64,28 @@ def over_vstup(uzivatel_vstup):
 
 def preved(uzivatel_vstup):
     """Prevede jednotliva pismena na arabske cislice """
-
+    arabske_cislice = []
     for i in range (len(uzivatel_vstup)):      #prevod zadaneho retezce na pole cisel
 
-        if rimske_cislo[i] =="I":
+        if uzivatel_vstup[i] =="I":
             arabske_cislice.append(1)
 
-        elif rimske_cislo[i] =="V":
+        elif uzivatel_vstup[i] =="V":
             arabske_cislice.append(5)
 
-        elif rimske_cislo[i] =="X":
+        elif uzivatel_vstup[i] =="X":
             arabske_cislice.append(10)
 
-        elif rimske_cislo[i] =="L":
+        elif uzivatel_vstup[i] =="L":
             arabske_cislice.append(50)
 
-        elif rimske_cislo[i] =="C":
+        elif uzivatel_vstup[i] =="C":
             arabske_cislice.append(100)
 
-        elif rimske_cislo[i] =="D":
+        elif uzivatel_vstup[i] =="D":
             arabske_cislice.append(500)
 
-        elif rimske_cislo[i] =="M":
+        elif uzivatel_vstup[i] =="M":
             arabske_cislice.append(1000)
 
     return arabske_cislice
@@ -102,6 +112,6 @@ def prepocitej(uzivatel_vstup):
         return ('Zadane cislo {} ma hodnotu {}.'.format(uzivatel_vstup, arabske_cislo + arabske_cislice[len(uzivatel_vstup)-1]))     #posledni cislice se vzdy pricita
 
 
-#hlavni program                              
+#hlavni program
 if over_vstup(rimske_cislo):            # Pokud je spravny vstup, zacina prepoct
     print(prepocitej(rimske_cislo))     # Tisk vysledneho cisla
