9c181f4e5359098630546c51d69101f242efcf89
Zuzana Filova <zuzana.filova@volny.cz>
2018-04-27 21:48:32 +0200

    Ke kontrole.

---
+++ Ukoly_slovniky/obraceny_slovnik.py
@@ -0,0 +1,19 @@
+# BONUS
+# vytvoř ze stávajího slovníku nový "obrácený" slovník,
+# kde klíčem bude vždy datum a hodnotou seznam jmen
+# (tj. počítej s tím, že jich může být u jednoho data více)
+
+narozeniny_obraceno = {}
+seznam = []
+
+for polozka in narozeniny.items():
+
+    if polozka[1] not in narozeniny_obraceno:
+        seznam.append(polozka[0])
+        narozeniny_obraceno[polozka[1]] = seznam
+        seznam = []
+    else:
+        narozeniny_obraceno[polozka[1]].append(polozka[0])
+
+
+print(narozeniny_obraceno)   
