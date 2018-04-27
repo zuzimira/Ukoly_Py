393e162a3a9234129a72690d821c157c34cb516e
Zuzana Filova <zuzana.filova@volny.cz>
2018-04-27 19:14:09 +0200

    Ke kontrole.

---
+++ Skautska_hra/skautska_hra.py
@@ -0,0 +1,35 @@
+import random
+
+moznosti = int(input('Kolik moznosti odpovedi na otazku budes vkladat? '))
+print()
+
+def odpovedi(pocet):
+    """vrati slovnik , kde klic je otazka a hodnota je seznam ruznych odpovedi"""
+    seznam_otazek = ['Jaky? ', 'Kdo? ', 'Co delal? ', 'Kde? ', 'Kdy? ', 'Proc? ']
+    zasoba_slov = {}
+    for otazka in seznam_otazek:
+        odpoved = []
+        while len(odpoved) < moznosti:
+            if otazka == 'Jaky? ':
+                slovo=input(otazka).title()
+
+            elif otazka == 'Kdy? ':
+                slovo = input(otazka)+','
+
+            elif otazka == 'Proc? ':
+                slovo = input(otazka)+'.'
+
+            else:
+                slovo = input(otazka)
+            odpoved.append(slovo)
+            zasoba_slov[otazka] = odpoved
+    return zasoba_slov
+
+
+def vypis_hru(slovnik):
+    """Vypise 'zertovnou vetu', kazde slovo nahodne vybere z moznosti"""
+    print("Nase veta zni:")
+    print()
+    for i in slovnik.values():
+        print('{} '.format(random.choice(i)), end = '')
+    print()    
