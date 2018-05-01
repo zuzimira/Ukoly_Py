e14d3d90791133d6f7de5457d79a72c88da6fd79
Zuzana Filova <zuzana.filova@volny.cz>
2018-05-01 10:43:29 +0200

    Pridany dve moznosti pro zefektivneni scitani znaku.

--- Ukoly_slovniky/uloha_2_3.py
+++ Ukoly_slovniky/uloha_2_3.py
@@ -7,6 +7,27 @@ def secti_znaky(retezec):
             slovnik_znaky[znak] = retezec.count(znak)
     return(slovnik_znaky)
 
+
+def secti_znaky(retezec):
+    """Vrati slovnik, kde klice jsou jednotlive znaky se zadaneho retezce
+    a hodnoty je jejich cetnost"""
+    slovnik_znaky = {}
+    for znak in retezec:
+        if znak not in slovnik_znaky:
+            slovnik_znaky[znak] = retezec.count(znak)
+    return(slovnik_znaky)
+
+
+def secti_znaky1(retezec):
+    """Vrati slovnik, kde klice jsou jednotlive znaky se zadaneho retezce
+    a hodnoty je jejich cetnost"""
+    znaky = set(retezec)
+    slovnik_znaky = {}
+    for znak in znaky:
+        slovnik_znaky[znak] = retezec.count(znak)
+    return(slovnik_znaky)
+
+
 def vypis_slovnik(slovnik):
     """Vypise polozky zadaneho slovniku, kazdou na jeden radek"""
     i = 1
@@ -21,3 +42,4 @@ def vypis_slovnik(slovnik):
 
 
 print(vypis_slovnik(secti_znaky('!kobyla ma maly bok!')))
+print(vypis_slovnik(secti_znaky1('!kobyla ma maly bok!')))
